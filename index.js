e7aa6c62f9ace46f3a8f04baac8570e6cd44b253
Zack Abrams <zackabrams@dyn-160-39-141-66.dyn.columbia.edu>
2019-09-05 16:49:39 -0400

    hooray for poop

---
+++ index.js
@@ -0,0 +1,87 @@
+
+var SlackBot = require('slackbots');
+var fs = require('fs');
+
+// create a bot
+
+const token1 = process.env.BWOGTOKEN
+console.log(token1);
+var bot = new SlackBot({
+  token: 'token1',
+  name: 'poopie'
+});
+
+var currentlyPooping = 0;
+
+bot.on('start', function() {
+    // more information about additional params https://api.slack.com/methods/chat.postMessage
+    var params = {
+        icon_emoji: ':poop:',
+        link_names: 'true'
+    };
+    daataa = bot.getUsers()._value.members
+    fs.writeFile('Output.txt', JSON.stringify(daataa), function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+    });
+    // define channel, where bot exist. You can adjust it there https://my.slack.com/services
+    bot.postMessageToChannel('shhhhtesting', "Hello! I'm starting.", params);
+    channellist = bot.getChannels();
+
+});
+
+function poopAlert() {
+    var params = {
+        icon_emoji: ':poop:',
+        link_names: 'true'
+    };
+
+    const { google } = require('googleapis')
+    const fs = require('fs')
+
+    const key = require('/Users/zackabrams/Downloads/bwogauth.json')
+    const scopes = 'https://www.googleapis.com/auth/analytics.readonly'
+    const jwt = new google.auth.JWT(
+      process.env.CLIENT_EMAIL,
+      null,
+      process.env.PRIVATE_KEY,
+      scopes
+      )
+    const view_id = '198211958'
+
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = '/Users/zackabrams/Downloads/bwogauth.json'
+
+    jwt.authorize((err, response) => {
+      google.analytics('v3').data.realtime.get(
+        {
+          auth: jwt,
+          ids: 'ga:' + view_id,
+          metrics: 'rt:activeUsers',
+          dimensions: 'rt:pageTitle'
+
+        },
+        (err, result) => {
+          console.log(result.data.rows)
+          var analData = result.data.rows
+          var string = analData.toString()
+          var poop = string.search("Poop")
+          console.log(poop)
+          if (poop == -1 && currentlyPooping == 0) {
+            currentlyPooping = 0;
+          } else if (poop == -1 && currentlyPooping == 1) {
+            currentlyPooping = 0;
+            bot.postMessageToChannel('shhhhtesting', "*Someone is done using Bwog to figure out where to poop*", params)
+            console.log("Someone is done using Bwog to figure out where to poop")
+          } else if (poop >= 0 && currentlyPooping == 0) {
+            currentlyPooping = 1;
+            bot.postMessageToChannel('shhhhtesting', "*Someone is using Bwog to figure out where to poop*", params)
+            console.log("Someone is using Bwog to figure out where to poop")
+          }
+        }
+
+      )
+    })
+
+};
+
+var interval = setInterval(function () { poopAlert(); }, 5000);
